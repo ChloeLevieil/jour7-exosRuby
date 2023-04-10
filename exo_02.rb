ca59204cf92abf878de9856d2fc635c0c1367f18
ChloeLevieil <chloe.levieil@outlook.fr>
2023-04-10 19:14:56 +0200

    3 premiers exos OK

---
+++ exo_02.rb
@@ -0,0 +1,3 @@
+puts "Hello, quel est ton prénom ?"
+user_name = gets.chomp 
+puts "Hello #{user_name}"
---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+puts "Quelle est ta date de naissance ?"
+print ">"
+birthyear = gets.chomp
+puts "En 2017, tu avais #{2017 - birthyear.to_i} ans"
