81ee3b77041e413410379d5a0b4847e7ed14ef6c
ChloeLevieil <chloe.levieil@outlook.fr>
2023-04-11 20:34:35 +0200

    dernière version

--- exo_15.rb
+++ exo_15.rb
@@ -1,10 +1,11 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+puts "Choisi  un numéro entre 1 et 25"
 print ">"
 
 rows = gets.chomp.to_i
 
 for row in 0..rows
-	row.times {print "*"}
+	row.times {print "#"}
 	puts
 end
 
